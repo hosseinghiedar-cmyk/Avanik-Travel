ad7e1b34644a5004ec3ca9c2e0274911c605a837
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-08 08:20:16 +0330

    Sprint 017: wire booking persistence foundation

--- wordpress/avanik/functions.php
+++ wordpress/avanik/functions.php
@@ -5,4 +5,8 @@ require_once __DIR__ . '/inc/Theme.php';
 require_once __DIR__ . '/inc/Booking.php';
 require_once __DIR__ . '/inc/BookingMeta.php';
 require_once __DIR__ . '/inc/BookingTemplate.php';
+require_once __DIR__ . '/inc/BookingRepository.php';
+require_once __DIR__ . '/inc/BookingSchema.php';
+require_once __DIR__ . '/inc/BookingLifecycle.php';
+\Avanik\BookingLifecycle::register();
 \Avanik\Theme::boot();
