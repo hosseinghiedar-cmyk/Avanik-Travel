fa84382284bff12f60d2288face34c32b1841c14
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-08 08:21:42 +0330

    Sprint 018: integrate booking service and actions

--- wordpress/avanik/functions.php
+++ wordpress/avanik/functions.php
@@ -8,5 +8,8 @@ require_once __DIR__ . '/inc/BookingTemplate.php';
 require_once __DIR__ . '/inc/BookingRepository.php';
 require_once __DIR__ . '/inc/BookingSchema.php';
 require_once __DIR__ . '/inc/BookingLifecycle.php';
+require_once __DIR__ . '/inc/BookingService.php';
+require_once __DIR__ . '/inc/BookingActions.php';
 \Avanik\BookingLifecycle::register();
+\Avanik\BookingActions::register();
 \Avanik\Theme::boot();
