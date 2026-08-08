4fbbfdae33939dd69b5964e95dba971a6b8c4213
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-08 08:10:33 +0330

    Sprint 016: register booking foundation

--- wordpress/avanik/functions.php
+++ wordpress/avanik/functions.php
@@ -2,4 +2,7 @@
 defined('ABSPATH') || exit;
 require_once __DIR__ . '/inc/Navigation.php';
 require_once __DIR__ . '/inc/Theme.php';
+require_once __DIR__ . '/inc/Booking.php';
+require_once __DIR__ . '/inc/BookingMeta.php';
+require_once __DIR__ . '/inc/BookingTemplate.php';
 \Avanik\Theme::boot();
