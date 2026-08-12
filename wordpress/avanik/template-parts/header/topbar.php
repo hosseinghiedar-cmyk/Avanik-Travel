dde159ad2f705ddac0720111cbc26739970c8d1e
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-12 23:17:19 +0330

    Polish Persian Avanik topbar with configurable contact and support

--- wordpress/avanik/template-parts/header/topbar.php
+++ wordpress/avanik/template-parts/header/topbar.php
@@ -1,15 +1,17 @@
 <?php
 defined('ABSPATH') || exit;
+$phone = class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('phone') : '021-12345678';
+$support = class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('support') : 'پشتیبانی ۲۴ ساعته';
 ?>
 <div class="av-topbar">
   <div class="av-container av-topbar__inner">
     <div class="av-topbar__right">
-      <a class="av-topbar__link" href="<?php echo esc_url(home_url('/')); ?>">صفحه اصلی</a>
       <a class="av-topbar__link" href="<?php echo esc_url(home_url('/contact')); ?>">تماس با ما</a>
+      <span class="av-topbar__link">☎ <?php echo esc_html($phone); ?></span>
     </div>
     <div class="av-topbar__left">
-      <a class="av-topbar__link" href="<?php echo esc_url(home_url('/login')); ?>">ورود / ثبت نام</a>
-      <span class="av-topbar__link">پشتیبانی ۲۴ ساعته</span>
+      <a class="av-topbar__link" href="<?php echo esc_url(home_url('/login')); ?>">♙ ورود / ثبت نام</a>
+      <span class="av-topbar__link">◷ <?php echo esc_html($support); ?></span>
     </div>
   </div>
 </div>
