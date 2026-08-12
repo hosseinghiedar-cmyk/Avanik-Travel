f6bc3848dde9d2364f7a9a378b3dd5b1a13d4a8e
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-13 00:41:11 +0330

    Simplify top bar and replace login text with account icon in main header

--- wordpress/avanik/template-parts/header/topbar.php
+++ wordpress/avanik/template-parts/header/topbar.php
@@ -1,17 +1,6 @@
 <?php
 defined('ABSPATH') || exit;
-$phone = class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('phone') : '021-12345678';
-$support = class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('support') : 'پشتیبانی ۲۴ ساعته';
+$phone=class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('phone') : '021-12345678';
+$support=class_exists('\\Avanik\\ThemeSettings') ? \Avanik\ThemeSettings::get('support') : 'پشتیبانی ۲۴ ساعته';
 ?>
-<div class="av-topbar">
-  <div class="av-container av-topbar__inner">
-    <div class="av-topbar__right">
-      <a class="av-topbar__link" href="<?php echo esc_url(home_url('/contact')); ?>">تماس با ما</a>
-      <span class="av-topbar__link">☎ <?php echo esc_html($phone); ?></span>
-    </div>
-    <div class="av-topbar__left">
-      <a class="av-topbar__link" href="<?php echo esc_url(home_url('/login')); ?>">♙ ورود / ثبت نام</a>
-      <span class="av-topbar__link">◷ <?php echo esc_html($support); ?></span>
-    </div>
-  </div>
-</div>
+<div class="av-topbar"><div class="av-container av-topbar__inner"><div class="av-topbar__right"><a class="av-topbar__link" href="<?php echo esc_url(home_url('/contact')); ?>">تماس با ما</a><span class="av-topbar__link">☎ <?php echo esc_html($phone); ?></span></div><div class="av-topbar__left"><span class="av-topbar__link">◷ <?php echo esc_html($support); ?></span></div></div></div>
