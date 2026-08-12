bb0c714f03a51c5908225e1c3a27c9f2df8a0ab5
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-12 20:40:25 +0330

    Enqueue Avanik frontend stylesheet in hosting theme

--- wordpress/avanik/header.php
+++ wordpress/avanik/header.php
@@ -1,5 +1,7 @@
 <?php
 defined('ABSPATH') || exit;
+wp_enqueue_style('avanik-theme', get_stylesheet_uri(), [], '0.4.0');
+wp_enqueue_style('avanik-frontend', get_template_directory_uri() . '/assets/css/avanik-theme.css', ['avanik-theme'], '0.4.0');
 get_template_part('template-parts/header/main');
 ?>
 <main id="primary" class="av-site-main">
