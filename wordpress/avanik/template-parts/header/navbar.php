850af9648bd98137468c43aaf151accd9c0a0a95
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-13 00:13:03 +0330

    Wire primary online booking CTA to flight search page

--- wordpress/avanik/template-parts/header/navbar.php
+++ wordpress/avanik/template-parts/header/navbar.php
@@ -2,7 +2,9 @@
   <div class="av-container">
     <div class="av-navbar__wrapper">
       <div class="av-navbar__logo">
-        <?php the_custom_logo(); ?>
+        <?php if (function_exists('the_custom_logo') && has_custom_logo()) : the_custom_logo(); else : ?>
+          <img src="<?php echo esc_url(get_template_directory_uri().'/assets/images/avanik-logo.svg'); ?>" alt="آوانیک پرواز آسیا">
+        <?php endif; ?>
       </div>
 
       <div class="av-navbar__menu">
@@ -17,7 +19,7 @@
       </div>
 
       <div class="av-navbar__actions">
-        <a href="#" class="av-btn av-btn--primary">رزرو آنلاین</a>
+        <a href="<?php echo esc_url(home_url('/flights')); ?>" class="av-btn av-btn--primary">رزرو آنلاین</a>
       </div>
     </div>
   </div>
