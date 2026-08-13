017aa8813308dec5300ce8440445bd692c0d88ce
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-13 13:37:47 +0330

    Fix WordPress head hook in modern Avanik header

--- wordpress/avanik/header.php
+++ wordpress/avanik/header.php
@@ -1,6 +1,6 @@
 <?php if (!defined('ABSPATH')) exit; ?><!doctype html>
 <html <?php language_attributes(); ?> dir="rtl">
-<head><meta charset="<?php bloginfo('charset'); ?>"><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+<head><meta charset="<?php bloginfo('charset'); ?>"><meta name="viewport" content="width=device-width, initial-scale=1"><?php wp_head(); ?></head>
 <body <?php body_class('avanik-site'); ?>>
 <?php wp_body_open(); ?>
 <header class="avanik-header">
