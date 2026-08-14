9d5c548042df26145047b0b04a32733c1d9e7c2b
hosseinghiedar-cmyk <hosseinghiedar@gmail.com>
2026-08-14 19:28:04 +0330

    feat: enable Elementor integration in theme

--- wordpress/avanik/functions.php
+++ wordpress/avanik/functions.php
@@ -2,4 +2,5 @@
 defined('ABSPATH') || exit;
 require_once __DIR__ . '/inc/Navigation.php';
 require_once __DIR__ . '/inc/Theme.php';
+require_once __DIR__ . '/inc/Elementor.php';
 \Avanik\Theme::boot();
